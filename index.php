f27d7e4efebcd328dec9c7768a09229b8d2e60e5
indianaraamancio <56450574+indianaraamancio@users.noreply.github.com>
2022-04-22 11:30:24 -0300

    Estrutura do login
    Não finalizada.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <main>
@@ -16,24 +17,22 @@
     <div> 
         <div>     
             <figure class="figure">
-            <img src="..." class="figure-img img-fluid rounded" alt="Logotipo">
-            <figcaption class="figure-caption text-end">Agenda de compromissos</figcaption>
+            <img src="img/login.jpg" class="figure-img" alt="Logotipo" id="imglogin">
             </figure>
         </div>
 
         <form action="php/autenticacao.php" method="POST">
-        <div>
-            <label for="email" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="admin@escritorio.com">
+        <div class="form-floating">          
+            <input type="email" class="form-control" id="email" name="email">
+            <label for="email">Insira seu e-mail </label>
         </div>
-        <div>
-            <label for="senha" class="form-label">Senha</label>
+        <div class="form-floating">                
             <input type="password" class="form-control" id="senha" name="senha" aria-describedy="senhaHelp">
-            <div id="senhaHelp" class="form-text">Não compartilhe sua senha com terceiros </div>
+            <label for="senha" class="form-label">Senha</label>
         </div>
         <div>
             <button type="reset" class="btn btn-secondary">Limpar</button>
-           <button type="submit" class="btn btn-primary">Entrar</button>           
+           <button type="submit" class="btn btn-success">Entrar</button>           
         </div>
 
         </form>
